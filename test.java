6a08663aa9dec98d2b831d49b3eb8733c5d0ff32
ALminMuratspahic <82029922+ALminMuratspahic@users.noreply.github.com>
2024-07-19 14:01:19 +0200

    Create test.java

---
+++ test.java
@@ -0,0 +1,10 @@
+package com.SistemZaPracenjeLokalnihDogadjaja.model;
+
+import java.util.Set;
+
+public class Category {
+
+    int category_id;
+    private String title;
+    private Set<Events> events;
+}
